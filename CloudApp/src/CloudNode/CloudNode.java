264751cf92a0648fcd67dcf11fbfefaa23258050
CoenHamers <coenhamers@gmail.com>
2015-10-26 12:46:18 +0100

    CloudNode, CloudClient and CloudScheduler

--- CloudApp/src/CloudNode/CloudNode.java
+++ CloudApp/src/CloudNode/CloudNode.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cloudapp;
+package CloudNode;
 
 /**
  *
  * @author Coen
  */
-public class CloudApp {
+public class CloudNode {
 
     /**
      * @param args the command line arguments
